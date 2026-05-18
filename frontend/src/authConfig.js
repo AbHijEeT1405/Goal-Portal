cfe7d896d74b773cc11412a3fb24d96373b2d37c
AbHijEeT1405 <abhijeetsonawane4u@gmail.com>
2026-05-18 19:02:03 +0000

    Microsoft redirect uri changes for live deployment

--- frontend/src/authConfig.js
+++ frontend/src/authConfig.js
@@ -2,7 +2,7 @@ export const msalConfig = {
   auth: {
     clientId: "5abb436e-08e0-4bba-8546-67b6b747694d",
     authority: "https://login.microsoftonline.com/87c94cab-ce30-429a-bb0f-898337f6ec50",
-    redirectUri: "http://localhost:5173",
+    redirectUri: window.location.origin,
   },
   cache: {
     cacheLocation: "sessionStorage",
